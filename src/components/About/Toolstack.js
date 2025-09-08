11cca5eb461680bb503b4c66209f52051e12ec57
Goldstein1365 <ogagospel9@gmail.com>
2025-09-08 02:31:27 +0100

    change avatar image

--- src/components/About/Toolstack.js
+++ src/components/About/Toolstack.js
@@ -1,5 +1,4 @@
 import React from "react";
-// import { Col, Row } from "react-bootstrap";
 import { Col, Row, OverlayTrigger, Tooltip } from "react-bootstrap";
 import {
   SiVisualstudiocode,
--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import myImg from "../../Assets/avatar.svg";
+import myImg from "../../Assets/Profile.jpg";
 import Tilt from "react-parallax-tilt";
 import { AiFillGithub, AiOutlineTwitter } from "react-icons/ai";
 import { FaLinkedinIn } from "react-icons/fa";
@@ -59,7 +59,7 @@ function Home2() {
           </Col>
           <Col md={4} className="myAvtar">
             <Tilt>
-              <img src={myImg} className="img-fluid" alt="avatar" />
+              <img src={myImg} style={{ width: "18em", borderRadius: "50%" }} className="img-fluid" alt="avatar" />
             </Tilt>
           </Col>
         </Row>
